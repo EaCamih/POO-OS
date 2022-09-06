2869dcb890801e796a4b8fb2a70bed7a0f82205e
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-09-06 08:58:29 -0300

    Creating project basics
    Creating project, Data Base and connecting to the MysQl Database

---
+++ OS_3A2.sql
@@ -0,0 +1,26 @@
+create database OS_3A2;
+use OS_3A2;
+
+create table usuario (
+	ID int unsigned auto_increment not null primary key,
+    nome varchar(80) default null,
+    email varchar(80) default null,
+    telefone varchar(18) default null,
+    setor int default null
+);
+
+create table setor (
+	id int unsigned auto_increment not null primary key,
+    descricao varchar(100) default null
+);
+
+create table tecnico (
+	id int unsigned auto_increment not null primary key,
+    nome varchar(80) default null,
+    especialidade int default null
+);
+
+create table especialidade (
+	id int unsigned auto_increment not null primary key,
+    especialidade varchar(100) default null
+);
