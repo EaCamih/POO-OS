1ffec3d94264d7393bfd69fe48466339eb0622dd
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-09-09 08:05:53 -0300

    Updating DTO files

---
+++ OS_3A2/DTO/Tecnico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_3A2.DTO
+{
+    internal class DTO_Tecnico
+    {
+        private string id, espec_id;
+        private string nome;
+
+        public string Id { get => id; set => id = value; }
+        public string Espec_id { get => espec_id; set => espec_id = value; }
+        public string Nome { get => nome; set => nome = value; }
+    }
+}
---
+++ OS_3A2/DTO/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_3A2.DTO
+{
+    internal class DTO_Usuario
+    {
+        private int id, id_setor;
+        private string nome, email, telefone;
+
+        public int Id { get => id; set => id = value; }
+        public int Id_setor { get => id_setor; set => id_setor = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+    }
+}
