d93b12704995a72946ed9040ce8296a1277fbbfb
MarcL <70085700+MarcLDev@users.noreply.github.com>
2022-09-13 08:51:08 -0300

    OS_130922

---
+++ OS_3A2/BLL/BLL_Tecnico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OS_3A2.DAL;
+using OS_3A2.DTO;
+using System.Data;
+
+namespace OS_3A2.BLL
+{
+    internal class BLL_Tecnico
+    {
+        ConexaoBD bd = new ConexaoBD();
+
+        public DataTable ConsultarTabela()
+        {
+            try
+            {
+                return bd.ConsultarTabelas("Select * from setor");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
