63fc8367972ac115d6d131142d9fc3f7e0a7159b
Camilla V <70085700+EaCamih@users.noreply.github.com>
2024-04-23 10:05:28 -0300

    Update Tecnico.cs

--- OS_3A2/DTO/Tecnico.cs
+++ OS_3A2/DTO/Tecnico.cs
@@ -10,30 +10,10 @@ namespace OS_3A2.DTO
     {
         private int id, espec_id;
         private string nome, email, senha;
-        public int Id
-        {
-            get => id;
-            set => id = value;
-        }
-        public int Espec_id
-        {
-            get => espec_id;
-            set => espec_id = value;
-        }
-        public string Nome
-        {
-            get => nome;
-            set => nome = value;
-        }
-        public string Email
-        {
-            get => email;
-            set => email = value;
-        }
-        public string Senha
-        {
-            get => senha;
-            set => senha = value;
-        }
+        public int Id { get => id; set => id = value; }
+        public int Espec_id { get => espec_id; set => espec_id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Senha { get => senha; set => senha = value; }
     }
 }
